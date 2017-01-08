fe29af455c670a1dd072caff62c70a412ecbe7f9
Evan <github@erosson.org>
2017-01-07 19:34:59 -0800

    test playfab-style input

--- test/unit/chunker.js
+++ test/unit/chunker.js
@@ -13,6 +13,11 @@ describe('chunker', () => {
     expect(chunker.encode('abcde').state1).to.equal('cd')
     expect(chunker.encode('abcde').state2).to.equal('e')
   })
+  it('handles playfabified decodes', () => {
+    let chunker = new Chunker(2)
+    expect(chunker.decode({state: {Value: 'ab'}}).state).to.equal('ab')
+    expect(chunker.decode({state: {Value: 'ab'}, state1: {Value: 'cd'}, state2: {Value: 'e'}}).state).to.equal('abcde')
+  })
   it('has expected keys', () => {
     expect(chunker.keys()).to.deep.equal(['state', 'state1', 'state2', 'state3', 'state4', 'state5', 'state6', 'state7', 'state8', 'state9'])
   })
